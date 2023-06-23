2fa028760a83a5c21f63e7c80ebb8bce70464ab6
LeonardoPaiv <leopaiva45@gmail.com>
2023-06-23 16:14:32 -0300

    exércicio 1 finalizado

--- tpm1/js/index.js
+++ tpm1/js/index.js
@@ -2,24 +2,68 @@ let products;
 let brands = new Set();
 let types = new Set();
 let isOrdered = -1;
+let isOrderedRating = -1;
+let isOrderedPrice = -1;
+const containe = document.getElementById("container");
 const productList = document.getElementById("products-card");
 const clearbutton = document.getElementById("clear");
 const orderbutton = document.getElementById("order");
+const orderText = document.getElementById("aphl-order");
 const inputName = document.getElementById("nameFilter");
 const brandDropdown = document.getElementById("brandsDropdown");
 const typeDropdown = document.getElementById("typeDropdown");
-const arrowButton = document.getElementById("arrowButton");
+const arrowButtonAlph = document.getElementById("arrowButton-alph");
+const ratingButton = document.getElementById("order-rating");
+const pricegButton = document.getElementById("order-price");
+const arrowButtonRating = document.getElementById("arrowButton-rating");
+const arrowButtonPrice = document.getElementById("arrowButton-price");
+
+const rating = (number) => {
+  if (number === null)
+    return `
+  <i class="fa fa-star"></i>
+  <i class="fa fa-star"></i>
+  <i class="fa fa-star"></i>
+  <i class="fa fa-star"></i>
+  <i class="fa fa-star"></i>
+  `;
+  let rating = [];
+  for (let i = 0; i < number; i++) {
+    rating.push('<i class="fa fa-star checked"></i>');
+  }
+  if (Number.isInteger(number))
+    return Object.assign(
+      new Array(5).fill('<i class="fa fa-star"></i>'),
+      rating
+    ).join("");
+  else {
+    rating[rating.length - 1] =
+      '<i class="fa-solid fa-star-half-stroke checked"></i>';
+    return Object.assign(
+      new Array(5).fill('<i class="fa fa-star"></i>'),
+      rating
+    ).join("");
+  }
+};
 
 const templateProduct = (prod) => {
   return `
-    <div class="product" id="${prod.name}">
+    <div class="product" id="${prod.id}">
+    <div style="margin-bottom: 5px;" class="tooltip">
+    <span class="tooltiptext">${
+      prod.rating ? prod.rating.toFixed(1) : "0.0"
+    }</span>
+    ${rating(prod.rating)}
+    </div>
         <img class="product-image" src="https:${prod.api_featured_image}">
         <h2>${prod.name}</h2>
         <div class="product-brands">
-            <span class="brand">${prod.brand ? prod.brand : "-"}</span>
-            <span class="price">${prod.price_sign ? prod.price_sign : "R$"}: ${
-    prod.price ? prod.price * 5.5 : "-"
-  }</span>
+            <span class="brand">${prod.brand ? prod.brand : ""}</span>
+            <span class="price">R$: ${
+              prod.price
+                ? (prod.price * 5.5).toFixed(2).replace(".", ",")
+                : "0,00"
+            }</span>
         </div>
     </div>
     `;
@@ -49,6 +93,7 @@ const getProducts = async () => {
     option.text = type;
     typeDropdown.add(option);
   }
+  adicionarEventClickAoProduct();
 };
 
 const clear = () => {
@@ -60,6 +105,7 @@ const clear = () => {
   inputName.value = "";
   brandDropdown.value = "";
   typeDropdown.value = "";
+  adicionarEventClickAoProduct();
 };
 
 const filterByName = () => {
@@ -72,6 +118,7 @@ const filterByName = () => {
     product += templateProduct(item);
   }
   productList.innerHTML = product;
+  adicionarEventClickAoProduct();
 };
 
 const filterByBrand = () => {
@@ -84,6 +131,7 @@ const filterByBrand = () => {
     product += templateProduct(item);
   }
   productList.innerHTML = product;
+  adicionarEventClickAoProduct();
 };
 
 const filterByType = () => {
@@ -96,12 +144,13 @@ const filterByType = () => {
     product += templateProduct(item);
   }
   productList.innerHTML = product;
+  adicionarEventClickAoProduct();
 };
 
 const orderByAlph = () => {
   let orderedProducts;
   if (isOrdered === 1 || isOrdered === -1) {
-    orderedProducts = [...products.sort((a, b) => {
+    orderedProducts = products.sort((a, b) => {
       if (a.name < b.name) {
         return -1;
       }
@@ -109,11 +158,12 @@ const orderByAlph = () => {
         return 1;
       }
       return 0;
-    })];
+    });
     isOrdered = 2;
-    arrowButton.className = "fas fa-arrow-down";
+    arrowButtonAlph.className = "fas fa-arrow-down";
+    orderText.textContent = "Z-A";
   } else {
-    orderedProducts = [...products.sort((a, b) => {
+    orderedProducts = products.sort((a, b) => {
       if (a.name > b.name) {
         return -1;
       }
@@ -121,20 +171,164 @@ const orderByAlph = () => {
         return 1;
       }
       return 0;
-    })];
+    });
     isOrdered = 1;
-    arrowButton.className = "fas fa-arrow-up";
+    arrowButtonAlph.className = "fas fa-arrow-up";
+    orderText.textContent = "A-Z";
   }
   let product = "";
   for (let item of orderedProducts) {
     product += templateProduct(item);
   }
   productList.innerHTML = product;
+  adicionarEventClickAoProduct();
 };
+
+const orderByPrice = () => {
+  let orderedProducts;
+  if (isOrderedPrice === 1 || isOrderedPrice === -1) {
+    orderedProducts = products.sort((a, b) => {
+      if (a.price < b.price) {
+        return -1;
+      }
+      if (a.price > b.price) {
+        return 1;
+      }
+      return 0;
+    });
+    isOrderedPrice = 2;
+    arrowButtonPrice.className = "fas fa-arrow-down";
+  } else {
+    orderedProducts = products.sort((a, b) => {
+      if (a.price > b.price) {
+        return -1;
+      }
+      if (a.price < b.price) {
+        return 1;
+      }
+      return 0;
+    });
+    isOrderedPrice = 1;
+    arrowButtonPrice.className = "fas fa-arrow-up";
+  }
+  let product = "";
+  for (let item of orderedProducts) {
+    product += templateProduct(item);
+  }
+  productList.innerHTML = product;
+  adicionarEventClickAoProduct();
+};
+
+const orderByRating = () => {
+  let orderedProducts;
+  if (isOrderedRating === 1 || isOrderedRating === -1) {
+    orderedProducts = products.sort((a, b) => {
+      if (a.rating < b.rating) {
+        return -1;
+      }
+      if (a.rating > b.rating) {
+        return 1;
+      }
+      return 0;
+    });
+    isOrderedRating = 2;
+    arrowButtonRating.className = "fas fa-arrow-down";
+  } else {
+    orderedProducts = products.sort((a, b) => {
+      if (a.rating > b.rating) {
+        return -1;
+      }
+      if (a.rating < b.rating) {
+        return 1;
+      }
+      return 0;
+    });
+    isOrderedRating = 1;
+    arrowButtonRating.className = "fas fa-arrow-up";
+  }
+  let product = "";
+  for (let item of orderedProducts) {
+    product += templateProduct(item);
+  }
+  productList.innerHTML = product;
+  adicionarEventClickAoProduct();
+};
+
+const adicionarEventClickAoProduct = () => {
+  const productItems = document.querySelectorAll(".product");
+  productItems.forEach(function (product) {
+    product.addEventListener("click", function (event) {
+      // Encontrar o elemento pai com a classe "product" mais próximo
+      var productElement = event.target.closest(".product");
+
+      // Verificar se foi encontrado um elemento pai com a classe "product"
+      if (productElement) {
+        // Obter o prod.id do elemento pai
+        var productId = productElement.id;
+        openDialog(productId);
+        // Realizar outras ações com o prod.id conforme necessário
+      }
+    });
+  });
+};
+
+const openDialog = async (prodId) => {
+  const prodi = await products.find(i => i.id == prodId);
+  let dialog = document.createElement('div');
+  dialog.id = 'dialog';
+  dialog.innerHTML = `
+<div id="dialog-container">
+  <div id="product-collumn">
+    <div class="product-atribute">
+      <label>Brand</label>
+      <p>${prodi.brand ? prodi.brand : ''}</p>
+    </div>
+    <div class="product-atribute">
+      <label>Price</label>
+      <p>R$: ${
+        prodi.price
+          ? (prodi.price * 5.5).toFixed(2).replace(".", ",")
+          : "0,00"
+      }</p>
+    </div>
+    <div class="product-atribute">
+      <label>Rating</label>
+      <p>${prodi.rating ? prodi.rating.toFixed(1) : "0.0"}</p>
+    </div>
+    <div class="product-atribute">
+      <label>Category</label>
+      <p>${prodi.category ? prodi.category : ""}</p>
+    </div>
+    <div class="product-atribute">
+      <label>Product type</label>
+      <p>${prodi.product_type ? prodi.product_type : ""}</p>
+    </div>
+  </div>
+  <img
+    class="product-image"
+    src="https:${prodi.api_featured_image}"
+  />
+  <button id="close-dialog">X</button>
+</div>
+  `;
+  document.querySelector('body').appendChild(dialog)
+  const closeButton = document.getElementById('close-dialog')
+  closeButton.addEventListener('click', removeDialog)
+  containe.classList.add('block')
+};
+
+const removeDialog = () => {
+  document.getElementById('dialog').remove();
+  containe.classList.remove('block')
+}
 
 clearbutton.addEventListener("click", clear);
 
 orderbutton.addEventListener("click", orderByAlph);
+
+ratingButton.addEventListener("click", orderByRating);
+
+pricegButton.addEventListener("click", orderByPrice);
 
 inputName.addEventListener("input", filterByName);
 
